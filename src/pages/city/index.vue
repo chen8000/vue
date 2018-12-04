2c70a407242a8d0acbdd1a4f27e4c4fa2ab44ee9
zhanghui.chen <13126618000@163.com>
2018-12-04 13:50:55 +0800

    city-search

--- src/pages/city/index.vue
+++ src/pages/city/index.vue
@@ -1,21 +1,24 @@
 <template>
     <div>
-        <Header/>
+        <Header />
+        <Search />
     </div>
 </template>
 
 <script>
 import Header from './components/header'
+import Search from './components/search'
 
 export default {
     name:'City',
     components:{
-        Header,
+        Header, 
+        Search
     }
 }
 </script>
 
 <style lang="stylus" scoped>
-    
+
 </style>
 
