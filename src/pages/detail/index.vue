8c54b45aa29459a31e792f875a3e875db4e190b1
zhanghui.chen <13126618000@163.com>
2018-12-05 16:58:56 +0800

    1

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -1,21 +1,28 @@
 <template>
     <div>
         <Banner/>
+        <Header/>
+        <div class="content">
+
+        </div>
     </div>
 </template>
 
 <script>
 import Banner from './components/banner'
+import Header from './components/header'
 export default {
     name:'Detail',
     components:{
-        Banner
+        Banner,
+        Header
     }
 }
 </script>
 
 <style lang="stylus" scoped>
-
+    .content
+        height: 50rem
 </style>
 
 
